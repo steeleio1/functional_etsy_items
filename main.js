fc18f7b49be82a15b3d4e480a440a0a676767665
Jason Steele <steeleio1@mac.com>
2016-06-17 15:19:07 -0400

    added some css style and first js

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var pricelookup = items.map(function(dog){                 
+	return dog.price});
+
+
+
+
+
+
+// var answer1_box = document.querySelector("#answer1")
+// undefined
+// answer1_box
+// <div class=​"answer" id=​"answer1">​</div>​
+// typeof answre
+// "undefined"
+// typeof answer1_box
+// "object"
+// answer1_box.innerHTML
+// ""
+// answer1_box.innerHTML = "<h1>Cool</h1>"
+// "<h1>Cool</h1>"
+// answer1_box
+// <div class=​"answer" id=​"answer1">​…​</div>​<h1>​Cool​</h1>​</div>​
+// answer1_box.innerHTML = ""
+// ""
+// items
+// [Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object, Object]
